7e4108b31f102b85f7270726cf53bd55cce9cd51
진석 임 <soos7165@jinseog-im-ui-imac.local>
2017-08-28 09:56:50 +0900

    iOS 어플

--- GoWith/ViewController.swift
+++ GoWith/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var myWebView: UIWebView!
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
 
+    func loadWebPage(_ url: String){
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.loadRequest(myRequest)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myWebView.delegate = self
+        loadWebPage("http://www.withhome360.com")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        myActivityIndicator.startAnimating()
+    }
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        myActivityIndicator.stopAnimating()
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack()
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myWebView.reload()
+    }
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward()
+    }
 
 }
 
